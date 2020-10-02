3eac77cb2b1de96d30ebeb06352a2ed38c855cf8
Gabriel758Eduardo <eduardosantos9406@gmail.com>
2020-10-02 17:46:51 -0300

    Calculadora em Java

---
+++ js/CalculadoraJava.js
@@ -0,0 +1,10 @@
+var numero1 = prompt("Digite o primeiro numero da soma:");
+var numero2 = prompt("Digite o segundo numero da soma:");
+
+var n1 = parseInt(numero1);
+var n2 = parseInt(numero2);
+var resultado = n1 + n2;
+
+console.log(resultado);
+
+alert("Resultado da soma eh:" + resultado);
